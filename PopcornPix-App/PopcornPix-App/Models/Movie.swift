3d200a768fa10010a20b5d9db3b160e08f148b0d
KirthiMaharaj055 <kirthimaharaj5@gmail.com>
2024-08-26 16:49:44 +0200

    Created movie model

---
+++ PopcornPix-App/PopcornPix-App/Models/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  PopcornPix-App
+//
+//  Created by Kirthi Maharaj on 2024/08/26.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let movieId: Int
+    let title: String
+    let releaseDate: String
+    let posterPath: String?
+}
+
+struct MovieResponse: Codable {
+    let results: [Movie]
+}
+
---
+++ PopcornPix-App/PopcornPix-App/Services/MovieService.swift
@@ -0,0 +1,12 @@
+//
+//  MovieService.swift
+//  PopcornPix-App
+//
+//  Created by Kirthi Maharaj on 2024/08/26.
+//
+
+import Foundation
+
+class MovieService {
+    
+}
