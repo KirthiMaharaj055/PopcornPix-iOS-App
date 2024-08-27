e16331c1cd56db59b8751654ac1245b6517239c3
KirthiMaharaj055 <kirthimaharaj5@gmail.com>
2024-08-27 12:33:08 +0200

    Connected the objects to the controller cell

--- PopcornPix-App/PopcornPix-App/ViewControllers/MovieTableViewCell.swift
+++ PopcornPix-App/PopcornPix-App/ViewControllers/MovieTableViewCell.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configure(withInfo movies: Movies){
+        self.movieTitle.text = movies.title
+        
+        let imageUrl = URL(string: "https://image.tmdb.org/t/p/w500\((movies.backdrop_path)!)")
+        if let dataImage = try? Data(contentsOf: imageUrl!)  {
+            self.movieImage.image = UIImage(data: dataImage)
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
